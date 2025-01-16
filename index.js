9381057f1e6f4ba312fe536ae9ab626ca3f3be64
Alisa <alisafavorskaa81@gmail.com>
2025-01-16 09:01:07 +0900

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+function multiply(a, b) {
+    return a * b;
+  }
+  console.log(multiply(3, 8));
+  console.log(multiply(1, 2));
+
+
+// //переменные
+// function repeat(str, num) {
+//     let result = '';
+//     for (let i = 0; i < num; i++) {
+//         result += str;
+//     }
+//     return result;
+// }
+// console.log(repeat('sometext', 2)); 
+// console.log(repeat('wo', 3));
